c14b73fb3c16bf49b8236ff84d0e1dda08b7a63b
aa <andrew@raw32.com>
2008-09-06 19:38:46 -0700

    new

--- rubyvideo.rb
+++ rubyvideo.rb
@@ -2,14 +2,18 @@ require 'rubygems'
 require 'gd2'
 
 include GD2
-image = Image.new(200,300)
-image.draw do |pen|
-  pen.color = image.palette.resolve Color[1.0, 0.75, 0.5]
-  pen.thickness = 2
-  pen.move_to 25, 50
-  pen.line_to 175, 50
-  pen.move -150, 25
-  pen.font = Font::TrueType['/System/Library/Fonts/teenbold.ttf', 20]
-  pen.text 'Hello, world!'
+
+100.times do |i|
+  puts i
+  image = Image.new(640,480)
+  image.draw do |pen|
+    pen.color = image.palette.resolve Color[1.0, 0.75, 0.5]
+    pen.thickness = 2
+    pen.move_to 25, 50
+    pen.line_to 175, 50
+    pen.move -150-i, 25+i
+    pen.font = Font::TrueType['/System/Library/Fonts/teenbold.ttf', 20]
+    pen.text 'Hello, world!'
+  end
+  image.export("/Users/aa/ginc/rubyvideo/seq/seq#{i}.png")
 end
-image.export('/Users/aa/ginc/rubyvideo/new-image.png')
